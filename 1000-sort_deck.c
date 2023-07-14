a13e391195b9870b86739d71ccf6571a90eb5132
ngugimuchangi <tech.curious14@gmail.com>
2023-07-14 17:06:29 +0300

    fixing betty styles

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,52 +1,52 @@
-
 #include <stdlib.h>
 #include "deck.h"
 
 /**
  * get_card_value - get the value of a card based on its kind and value
  *
- * @param card: pointer to the card to get the value of
+ * @card: pointer to the card to get the value of
  * Return: int - value of the card
  */
 int get_card_value(const card_t *card)
 {
+	int card_value = card->kind * 13;
 
-    int card_value = card->kind * 13;
-    switch (card->value[0])
-    {
-    case 'A':
-        card_value += 1;
-        break;
-    case 'J':
-        card_value += 11;
-        break;
-    case 'Q':
-        card_value += 12;
-        break;
-    case 'K':
-        card_value += 13;
-        break;
-    case '1':
-        card_value += (int)card->value[0] * 10 + (int)card->value[1] - 96;
-        break;
-    default:
-        card_value += (int)card->value[0] - 48;
-        break;
-    }
-    return card_value;
+	switch (card->value[0])
+	{
+	case 'A':
+		card_value += 1;
+		break;
+	case 'J':
+		card_value += 11;
+		break;
+	case 'Q':
+		card_value += 12;
+		break;
+	case 'K':
+		card_value += 13;
+		break;
+	case '1':
+		card_value += (int)card->value[0] * 10 + (int)card->value[1] - 96;
+		break;
+	default:
+		card_value += (int)card->value[0] - 48;
+		break;
+	}
+	return (card_value);
 }
 
 /**
  * compare - compare two nodes in a doubly linked list
  * @node_one: first node to compare
  * @node_two: second node to compare
- * @return int - difference between the two nodes
+ * Return: int - difference between the two nodes
  */
 int compare(deck_node_t *node_one, deck_node_t *node_two)
 {
-    int card_one_value = get_card_value(node_one->card);
-    int card_two_value = get_card_value(node_two->card);
-    return card_one_value - card_two_value;
+	int card_one_value = get_card_value(node_one->card);
+	int card_two_value = get_card_value(node_two->card);
+
+	return (card_one_value - card_two_value);
 }
 
 /**
@@ -57,24 +57,26 @@ int compare(deck_node_t *node_one, deck_node_t *node_two)
  * @node_two: second node to swap
  * Return: nothing
  */
-void swap_nodes(deck_node_t **head, deck_node_t *node_one, deck_node_t *node_two)
+void swap_nodes(deck_node_t **head, deck_node_t *node_one,
+				deck_node_t *node_two)
 {
-    deck_node_t *temp = node_one->prev;
-    node_one->next = node_two->next;
-    if (node_two->next)
-        node_two->next->prev = node_one;
+	deck_node_t *temp = node_one->prev;
 
-    if (node_two->prev == node_one)
-        node_one->prev = node_two;
-    else
-        node_one->prev = node_two->prev;
+	node_one->next = node_two->next;
+	if (node_two->next)
+		node_two->next->prev = node_one;
 
-    node_one->prev->next = node_one;
-    node_one->prev->prev = temp;
-    if (temp)
-        temp->next = node_two;
-    else
-        *head = node_two;
+	if (node_two->prev == node_one)
+		node_one->prev = node_two;
+	else
+		node_one->prev = node_two->prev;
+
+	node_one->prev->next = node_one;
+	node_one->prev->prev = temp;
+	if (temp)
+		temp->next = node_two;
+	else
+		*head = node_two;
 }
 
 /**
@@ -86,23 +88,23 @@ void swap_nodes(deck_node_t **head, deck_node_t *node_one, deck_node_t *node_two
  */
 void sort_deck(deck_node_t **deck)
 {
-    deck_node_t *deck_tracker, *deck_back_tracker, *current_node, *temp;
+	deck_node_t *deck_tracker, *deck_back_tracker, *current_node, *temp;
 
-    if (!(*deck))
-        return;
+	if (!(*deck))
+		return;
 
-    deck_tracker = (*deck)->next;
+	deck_tracker = (*deck)->next;
 
-    while (deck_tracker)
-    {
-        temp = deck_tracker->next;
-        current_node = deck_tracker;
-        deck_back_tracker = deck_tracker->prev;
-        while (deck_back_tracker && compare(current_node, deck_back_tracker) < 0)
-        {
-            swap_nodes(deck, deck_back_tracker, current_node);
-            deck_back_tracker = current_node->prev;
-        }
-        deck_tracker = temp;
-    }
+	while (deck_tracker)
+	{
+		temp = deck_tracker->next;
+		current_node = deck_tracker;
+		deck_back_tracker = deck_tracker->prev;
+		while (deck_back_tracker && compare(current_node, deck_back_tracker) < 0)
+		{
+			swap_nodes(deck, deck_back_tracker, current_node);
+			deck_back_tracker = current_node->prev;
+		}
+		deck_tracker = temp;
+	}
 }
