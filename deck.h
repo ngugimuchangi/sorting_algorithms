8e842bcc7e3ccc0c9ac32a8719e67f26a55fc897
ngugimuchangi <tech.curious14@gmail.com>
2023-07-14 17:17:06 +0300

    updating betty style compliance in header file

--- deck.h
+++ deck.h
@@ -1,11 +1,18 @@
 #ifndef DECK_H
 #define DECK_H
+/**
+ * enum kind_e - Suit of a card
+ * @SPADE: 0, Spade suit
+ * @HEART: 1, Heart suit
+ * @CLUB: 2, Club suit
+ * @DIAMOND: 3, Diamond suit
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -17,8 +24,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	 const char *value;
+	 const kind_t kind;
 } card_t;
 
 /**
@@ -30,9 +37,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	 const card_t *card;
+	 struct deck_node_s *prev;
+	 struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
